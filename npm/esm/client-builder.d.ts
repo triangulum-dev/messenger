5bf8684229d0f73e23ed726ceb5052d137a0ccec
grant-wilson <grantm.wilsonii@gmail.com>
2025-05-11 18:41:01 -0400

    Update dev container. Rebuild npm.

--- npm/esm/client-builder.d.ts
+++ npm/esm/client-builder.d.ts
@@ -4,8 +4,8 @@ export declare class ClientBuilder<T extends object = object> {
     readonly id: string;
     readonly target: MessageTarget & MessageSource;
     constructor(id: string, target: MessageTarget & MessageSource);
-    addPromiseFunction<Name extends string, Args extends unknown[], ReturnType>(name: Name): ClientBuilder<T & AddFunctionType<Name, Args, ReturnType>>;
-    addObservableFunction<Name extends string, Args extends unknown[], ReturnType>(name: Name): ClientBuilder<T & AddObservableFunctionType<Name, Args, ReturnType>>;
+    addPromiseFunction<Args extends unknown[], ReturnType, Name extends string>(name: Name): ClientBuilder<T & AddFunctionType<Name, Args, ReturnType>>;
+    addObservableFunction<Args extends unknown[], ReturnType, Name extends string>(name: Name): ClientBuilder<T & AddObservableFunctionType<Name, Args, ReturnType>>;
     build(): T;
 }
 //# sourceMappingURL=client-builder.d.ts.map
--- src/client-builder.ts
+++ src/client-builder.ts
@@ -1,5 +1,10 @@
 import { Client } from "./client.ts";
-import type { AddFunctionType, AddObservableFunctionType, MessageSource, MessageTarget } from "./model.ts";
+import type {
+  AddFunctionType,
+  AddObservableFunctionType,
+  MessageSource,
+  MessageTarget,
+} from "./model.ts";
 import { ProxyBuilder } from "./proxy-builder.ts";
 import type { Observable } from "rxjs";
 import {
@@ -14,18 +19,14 @@ export class ClientBuilder<T extends object = object> {
 
   constructor(
     readonly id: string,
-    readonly target: MessageTarget & MessageSource,
+    readonly target: MessageTarget & MessageSource
   ) {
     this.#proxyBuilder = new ProxyBuilder<T>();
     this.#client = new Client(id, target);
   }
 
-  addPromiseFunction<
-    Name extends string,
-    Args extends unknown[],
-    ReturnType,
-  >(
-    name: Name,
+  addPromiseFunction<Args extends unknown[], ReturnType, Name extends string>(
+    name: Name
   ): ClientBuilder<T & AddFunctionType<Name, Args, ReturnType>> {
     this.#proxyBuilder.addFunction<Args, Promise<ReturnType>>({
       name,
@@ -41,11 +42,11 @@ export class ClientBuilder<T extends object = object> {
   }
 
   addObservableFunction<
-    Name extends string,
     Args extends unknown[],
     ReturnType,
+    Name extends string
   >(
-    name: Name,
+    name: Name
   ): ClientBuilder<T & AddObservableFunctionType<Name, Args, ReturnType>> {
     this.#proxyBuilder.addFunction<Args, Observable<ReturnType>>({
       name,
